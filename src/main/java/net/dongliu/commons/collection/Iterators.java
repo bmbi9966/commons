5343757179023b848f193c331275c73a930d5d65
dongliu <dongliu@kuaishou.com>
2018-04-25 11:21:37 +0800

    add Iterators utils

---
+++ src/main/java/net/dongliu/commons/collection/Iterators.java
@@ -0,0 +1,59 @@
+package net.dongliu.commons.collection;
+
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Iterator;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+import static java.util.Objects.requireNonNull;
+
+/**
+ * Utils methods for Iterator
+ */
+public class Iterators {
+
+    /**
+     * Wrap iterator as Stream
+     *
+     * @param it  the iterator
+     * @param <T> the element type
+     * @return Stream contains remaining data in iterator
+     */
+    public static <T> Stream<T> stream(Iterator<T> it) {
+        requireNonNull(it);
+        return Iterables.stream(() -> it);
+    }
+
+    /**
+     * Return next element data of iterator. If not has next element, return Optional.empty().
+     * The element in Iterator cannot be null, or NullPointerException will be thrown.
+     *
+     * @param it  the iterator
+     * @param <T> the element type
+     * @return Optional contains the next element of iterator.
+     */
+    public static <T> Optional<T> next(Iterator<T> it) {
+        requireNonNull(it);
+        if (it.hasNext()) {
+            return Optional.of(it.next());
+        }
+        return Optional.empty();
+    }
+
+    /**
+     * Return next element data of iterator. If not has next element, return null.
+     *
+     * @param it  the iterator
+     * @param <T> the element type
+     * @return the next element of iterator.
+     */
+    @Nullable
+    public static <T> T nextOrNull(Iterator<T> it) {
+        requireNonNull(it);
+        if (it.hasNext()) {
+            return it.next();
+        }
+        return null;
+    }
+}
---
+++ src/test/java/net/dongliu/commons/collection/IteratorsTest.java
@@ -0,0 +1,35 @@
+package net.dongliu.commons.collection;
+
+import org.junit.Test;
+
+import java.util.Iterator;
+import java.util.Optional;
+
+import static org.junit.Assert.*;
+
+public class IteratorsTest {
+
+    @Test
+    public void stream() {
+        Iterator<Integer> iterator = Lists.of(1, 2).iterator();
+        assertEquals(2, Iterators.stream(iterator).count());
+    }
+
+    @Test
+    public void next() {
+        Iterator<Integer> iterator = Lists.of(1, 2).iterator();
+
+        assertEquals(Optional.of(1), Iterators.next(iterator));
+        assertEquals(Optional.of(2), Iterators.next(iterator));
+        assertEquals(Optional.empty(), Iterators.next(iterator));
+    }
+
+    @Test
+    public void nextOrNull() {
+        Iterator<Integer> iterator = Lists.of(1, 2).iterator();
+
+        assertEquals(Integer.valueOf(1), Iterators.nextOrNull(iterator));
+        assertEquals(Integer.valueOf(2), Iterators.nextOrNull(iterator));
+        assertNull(Iterators.nextOrNull(iterator));
+    }
+}
