2a85c503bffac268204e54dd04011ac7c2f276e0
dongliu <dongliu@kuaishou.com>
2018-08-29 15:59:26 +0800

    add file utils

--- src/main/java/net/dongliu/commons/io/Files2.java
+++ src/main/java/net/dongliu/commons/io/Files2.java
@@ -3,6 +3,7 @@ package net.dongliu.commons.io;
 import java.io.IOException;
 import java.io.UncheckedIOException;
 import java.nio.charset.Charset;
+import java.nio.file.CopyOption;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
@@ -12,6 +13,42 @@ import static java.nio.charset.StandardCharsets.UTF_8;
  * More utils for file
  */
 public class Files2 {
+
+    /**
+     * Copy source file to target file, make target dirs if need.
+     * If target file already exits, override it.
+     */
+    public static void copyFile(Path source, Path target, CopyOption... copyOptions) {
+        Path targetDir = target.getParent();
+        if (!Files.exists(targetDir)) {
+            if (!targetDir.toFile().mkdirs()) {
+                throw new UncheckedIOException(new IOException("make target file dirs failed"));
+            }
+        }
+        try {
+            Files.copy(source, target, copyOptions);
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
+
+    /**
+     * Copy source file to target file, make target dirs if need.
+     * If target file already exits, override it.
+     */
+    public static void moveFile(Path source, Path target, CopyOption... copyOptions) {
+        Path targetDir = target.getParent();
+        if (!Files.exists(targetDir)) {
+            if (!targetDir.toFile().mkdirs()) {
+                throw new UncheckedIOException(new IOException("make target file dirs failed"));
+            }
+        }
+        try {
+            Files.move(source, target, copyOptions);
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
 
     /**
      * Read all file data, to string.
