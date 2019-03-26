35ec492da121870e99ef9399052419e1bc0bae7e
Liu Dong <dongliu@live.cn>
2019-03-26 13:53:22 +0800

    add closeAll

--- src/main/java/net/dongliu/commons/io/Closeables.java
+++ src/main/java/net/dongliu/commons/io/Closeables.java
@@ -1,7 +1,12 @@
 package net.dongliu.commons.io;
 
 
+import org.checkerframework.checker.nullness.qual.NonNull;
 import org.checkerframework.checker.nullness.qual.Nullable;
+
+import java.util.List;
+
+import static java.util.Objects.requireNonNull;
 
 /**
  * Utils for deal with Closeables and AutoCloseables
@@ -28,13 +33,59 @@ public class Closeables {
      *
      * @param closeables the instances to be close.
      */
-    public static void closeQuietly(@Nullable AutoCloseable... closeables) {
+    public static void closeQuietly(@Nullable AutoCloseable @NonNull ... closeables) {
         for (AutoCloseable closeable : closeables) {
             if (closeable != null) {
                 try {
                     closeable.close();
                 } catch (Exception ignore) {
                 }
+            }
+        }
+    }
+
+    /**
+     * Close all resources. If exception is throw when close resource, it is added to Suppressed exception list of passed in Throwable.
+     * This behave just as try-with resource management.
+     * This method would not rethrow the passed in throwable, callers should rethrow it if needed.
+     *
+     * @param throwable  the origin exception
+     * @param closeables the resources to close
+     */
+    public static void closeAll(@NonNull Throwable throwable, @Nullable AutoCloseable @NonNull ... closeables) {
+        requireNonNull(throwable);
+        requireNonNull(closeables);
+        for (AutoCloseable closeable : closeables) {
+            if (closeable == null) {
+                continue;
+            }
+            try {
+                closeable.close();
+            } catch (Exception e) {
+                throwable.addSuppressed(e);
+            }
+        }
+    }
+
+    /**
+     * Close all resources. If exception is throw when close resource, it is added to Suppressed exception list of passed in Throwable.
+     * This behave just as try-with resource management.
+     * This method would not rethrow the passed in throwable, callers should rethrow it if needed.
+     *
+     * @param throwable  the origin exception
+     * @param closeables the resources to close
+     */
+    public static void closeAll(@NonNull Throwable throwable, @NonNull List<@Nullable AutoCloseable> closeables) {
+        requireNonNull(throwable);
+        requireNonNull(closeables);
+        for (AutoCloseable closeable : closeables) {
+            if (closeable == null) {
+                continue;
+            }
+            try {
+                closeable.close();
+            } catch (Exception e) {
+                throwable.addSuppressed(e);
             }
         }
     }
